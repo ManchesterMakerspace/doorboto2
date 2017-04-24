d5594d1efe8b1cdb1dbcabf12764bd52630cfd6a
Paul Beaudet <inof8or@gmail.com>
2017-04-23 22:36:52 -0400

    What can I say, I changed my mind how everything works to not requuire and external helper

--- doorboto2.js
+++ doorboto2.js
@@ -1,42 +1,182 @@
 // doorboto2.js ~ Copyright 2017 Manchester Makerspace ~ License MIT
+
+function genericErrorHandler(error, onNoError){
+    if(error){console.error('Im so sorry I had this error:' + error);}
+    else     {onNoError();}
+}
+
 var auth = {
     storage: require('node-persist'),
-    init: function(){                    // kinda a redundant shortcut but maybe there could be more we want to start up
-        return auth.storage.init();      // returns a promise that datastore is ready
+    init: function(){                     // kinda a redundant shortcut but maybe there could be more we want to start up
+        return auth.storage.init();       // returns a promise that datastore is ready
     },
-    updateMember: function(member){
-        if(member.lost === undefined)   {member.lost=false;}    // defualt to false if nothing was passed
-        if(member.revoked === undefined){member.revoked=false;} // defualt to false if nothing was passed
-        return auth.storage.setItem(member.cardID, {'fullname': member.fullname, 'expirationTime': member.expirationTime,
-                                                    'lost': member.lost, 'revoked': member.revoked});
+    updateCreateMember: function(member){ // setItem works like and upsert, this also creates members
+        var consolidatedMemberInfo = {
+            'fullname':       member.fullname,
+            'expirationTime': member.expirationTime
+        };
+        return auth.storage.setItem(member.cardID, consolidatedMemberInfo);
     },
     removeMember: function(member){
         return auth.storage.removeItem(member.cardID);
     },
     orize: function(cardID, onSuccess, onFail){
-        var stranger = true;
+        var inQuestion = { // default member in question
+            fullname: 'unregistered card',
+            cardID: cardID,
+            expirationTime: 0
+        };
         auth.storage.forEach(function(key, member){
             if(key === cardID){
-                stranger = false;
-                if(member.lost || member.revoked){
-                    if(member.lost){
-                        onFail(member.fullname + "'s lost card was used");        // message for cards flagged as lost
-                    } else {
-                        onFail(member.fullname + ' see board about card access'); // message for members with revoked flag
-                    }
-                } else {
-                    if(new Date().getTime() > new Date(member.expirationTime).getTime()){
-                        onSuccess(member.fullname);
-                    } else {
-                        onFail(member.fullname + "'s card needs to be renewed");
-                    }
+                inQuestion = member;         // hold on to info we have about this member in case we need to check source of truth and fail
+                inQuestion.cardID = cardID;  // add cardID property to check against database
+                if( new Date().getTime() > new Date(member.expirationTime).getTime()){
+                    inQuestion = false;      // member is good to go no check needed
+                    // TODO the issue with this is that if you have a member thats been revoked they are still going to be let in until the proposed cron runs
+                    onSuccess(member.fullname);
                 }
             }
         });
-        if(stranger){
-            socket.io.emit('regMember', {cardID: cardID}); // emit reg info to admin
-            onFail('Unregistered Card');
+        mongo.connectAndDo(auth.sourceOfTruthCheck(inQuestion, onSuccess, onFail), auth.canNotInfo(inQuestion, onFail));
+    },
+    sourceOfTruthCheck: function(onHandMember, onSuccess, onFail){
+        return function onConnectToMongo(){
+            mongo.member.findOne({'cardID': onHandMember.cardID}, function onFindMember(error, sourceMember){
+                genericErrorHandler(error, function givenNoError(){ // there are only unexpected results
+                    if(sourceMember){
+                        if(onHandMember.expirationTime){
+                            // member has already been let in if we have their expiration date at this point
+                        } else {
+                            // TODO case for lost and rejected cardIDs
+                            if( new Date().getTime() > new Date(sourceMember.expirationTime).getTime()){ // TODO migration for group members
+                                onSuccess(sourceMember.fullname);
+                            } else { // given member is expired
+                                onFail(sourceMember.fullname + ' has expired');
+                            }
+                        }
+                        updateCreateMember(sourceMember);           // update local data store
+                    } else {
+                        // TODO save rejected card to rejected card collection
+                        onFail('unregistered card');
+                    }
+                });
+            });
+        };
+    },
+    canNotInfo: function(member, onFail){
+        return function canNotConnectToMongo(){
+            if(member){
+                onFail('Could not check db for member: ' + member.fullname);
+            } // else member has already been let in anyhow
+        };
+    },
+
+};
+
+var mongo = { // depends on: mongoose
+    uri: process.env.MONGO_URI,
+    ose: require('mongoose'),
+    options: null,                                                       // this is where one would normally put auth info and so on
+    Schema: mongo.oseSchema,
+    member: mongo.ose.model('member', new mongo.Schema({                        // Member collection schema: Read only by doorboto -- Write only by interface
+        id: mongo.Schema.ObjectId,                                              // unique id of document
+        fullname: {type: String, required: '{PATH} is required', unique: true}, // full name of user
+        cardID: {type: String, required: '{PATH} is required', unique: true},   // user card id                                        TODO deprecate
+        status: {type: String, Required: '{PATH} is required'},                 // type of account, admin, mod, ect                    TODO deprecate
+        accesspoints: [String],                                                 // points of access member (door, machine, ect)        TODO depracate
+        expirationTime: {type: Number},                                         // pre-calculated time of expiration                   TODO depracate
+        groupName: {type: String},                                              // potentially member is in a group/partner membership TODO depracate
+        groupKeystone: {type: Boolean},                                         // notes who holds expiration date for group           TODO depracate
+        groupSize: {type: Number},                                              // notes how many members in group given in one        TODO depracate
+        password: {type: String},                                               // for admin cards only
+        email: {type: String},                                                  // store email of member for prosterity sake
+        slackHandle: {type: String},                                            // store slack username
+        notificationAck: {type: Boolean},                                       // recognizes a notification was sent out
+        expiredAck: {type: Boolean},                                            // recognizes doorboto was updated with expiration
+        cardToken: {type: String, unique: true},                                // TODO add 8 byte QID to write to key cards
+        // pick a random 4 byte number and convert to hex, try to write, try again if not unique
+    })),
+    cards: mongo.ose.model('card', new mongo.Schema({                           // Read only by doorboto -- Write only by Interface
+        id: mongo.Schema.ObjectId,
+        cardID: {type: String, required: '{PATH} is required', unique: true},   // UID of card, collection find key
+        memberName: {type: String, required: '{PATH} is required'},             // for quickly leaving messages about dated cards without looking up member Maybe not needed
+        memberID: {type: String, required: '{PATH} is required'},               // _id of member object fastest way to acurately look up a member
+        cardToken: {type: String},                                              // 8 byte QID unique to member, proposing to use in conjunction with UID
+        expirationTime: {type: Number},                                         // expiration of this member
+        validity: {type: String, required: '{PATH} is required'}                // active, expired, revoked, lost, stolen
+    })),
+    rejections: mongo.ose.model('rejections', new mongo.Schema({                // list of rejected cards, interface can grab last to register new members instead of using socket.io Write only by doorboto
+        id: mongo.Schema.ObjectId,
+        cardID: {type: String, required: '{PATH} is required'},
+        timeOf: {type: Date, default: Date.now},
+    })),
+    connectAndDo: function(success, fail){
+        mongo.ose.connect(mongo.uri, mongo.options, function onConnect(error){
+            if(error){
+                console.error('Mongo issue:' + error);
+                fail(error);
+            } else {
+                success();
+                mongo.ose.connection.close(); // close connection when we are done
+            }
+        });
+    }
+    /* init: function(){ // just
+        var Schema = mongo.ose.Schema; var ObjectId = Schema.ObjectId;
+        mongo.member = mongo.ose.model('member', new Schema({                         // create user object property
+            id: ObjectId,                                                             // unique id of document
+            fullname: { type: String, required: '{PATH} is required', unique: true }, // full name of user
+            cardID: { type: String, required: '{PATH} is required', unique: true },   // user card id
+            status: {type: String, Required: '{PATH} is required'},                   // type of account, admin, mod, ect
+            accesspoints: [String],                                                   // points of access member (door, machine, ect)
+            expirationTime: {type: Number},                                           // pre-calculated time of expiration
+            groupName: {type: String},                                                // potentially member is in a group/partner membership
+            groupKeystone: {type: Boolean},                                           // notes who holds expiration date for group
+            groupSize: {type: Number},                                                // notes how many members in group given in one
+            password: {type: String},                                                 // for admin cards only
+            email: {type: String},                                                    // store email of member for prosterity sake
+            slackHandle: {type: String},                                              // store slack username
+            notificationAck: {type: Boolean},                                         // recognizes a notification was sent out
+            expiredAck: {type: Boolean}                                               // recognizes doorboto was updated with expiration
+        }));
+    }, */
+};
+
+var slack = {
+    io: require('socket.io-client'),                         // to connect to our slack intergration server
+    firstConnect: false,
+    connected: false,
+    init: function(intergrationServer, authToken){
+        try {
+            slack.io = slack.io(intergrationServer); // slack https server
+            slack.firstConnect = true;
+        } catch (error){
+            console.log('could not connect to ' + intergrationServer + ' cause:' + error);
+            setTimeout(slack.init, 60000); // try again in a minute maybe we are disconnected from the network
         }
+        if(slack.firstConnect){
+            slack.io.on('connect', function authenticate(){  // connect with masterslacker
+                slack.io.emit('authenticate', {
+                    token: authToken,
+                    slack: {
+                        username: 'Doorboto2',
+                        // channel: 'whos_at_the_space',
+                        channel: 'test_channel',
+                        iconEmoji: ':robot_face:'
+                    }
+                }); // its important lisner know that we are for real
+                slack.connected = true;
+            });
+            slack.io.on('disconnect', function disconnected(){slack.connected = false;});
+        }
+    },
+    send: function(msg){
+        if(slack.connected){ slack.io.emit('msg', msg);
+        } else { console.log('404:'+msg); }
+    },
+    pm: function(handle, msg){
+        if(slack.connected){ slack.io.emit('pm', {userhandle: handle, msg: msg});
+        } else { console.log('404:'+msg);}
     }
 };
 
@@ -72,63 +212,9 @@ var arduino = {                        // does not need to be connected to and a
     }
 };
 
-var service = { // logic for adding a removing bot integrations
-    s: [], // array where we store properties and functions of connected sevices
-    create: function(name, socketId){
-        service.s.push({
-            socketId: socketId,
-            name: name
-        });
-        console.log(botProperties.username + ' just connected');
-    },
-    disconnect: function(socketId){                                                // hold socketId information in closure
-        return function socketDisconnect(){
-            service.do(socketId, function removeService(index){
-                var UTCString = new Date().toUTCString();                           // get a string of current time
-                console.log(service.s[index].username+' disconnecting '+UTCString); // give a warning when a bot is disconnecting
-                service.s.splice(index, 1);                                         // given its there remove bot from bots array
-            });
-        };
-    },
-    do: function(socketId, foundCallback){               // executes a callback with one of our bots based on socket id
-        var serviceNumber = service.s.map(function(eachService){
-            return eachService.socketId;
-        }).indexOf(socketId);                            // figure index bot in our bots array
-        if(clietNumber > -1){                            // NOTE we remove bots keeping ids in closure would be inaccurate
-            foundCallback(serviceNumber);                 // part where do happens
-        } else {
-            console.log(socketId + ':found no bot?');    // service is not there? Should never happen but w.e.
-        }
-    }
-};
-
-var socket = {                                                         // depends on slack, register, search, auth: handle socket events
-    io: require('socket.io'),
-    listen: function(server){
-        socket.io = sockets.io(server);
-        socket.io.on('connection', function(client){                   // when any socket connects to us
-            console.log('client connected:'+ client.id);               // notify when clients get connected to be assured good connections
-            client.on('authenticate', socket.auth(client));            // initially clients can only ask to authenticate
-        });
-    },
-    auth: function(client){                                                   // hold socketObj/key in closure, return callback to authorize user
-        return function(authPacket){                                          // data passed from service {token:"valid token", name:"of service"}
-            if(authPacket.token === process.env.DOORBOTO_TOKEN && authPacket.name){  // make sure we are connected w/ a trusted source with a name
-                service.create(authPacket.name, client.id);                          // returns number in bot array
-                client.on('disconnect', service.disconnect(client.id));              // remove service from service array on disconnect
-            } else {                                                                 // in case token was wrong or name not provided
-                console.log('Rejected socket connection: ' + client.id);
-                client.on('disconnect', function(){
-                    console.log('Rejected socket disconnected: ' + client.id);
-                });
-            }
-        };
-    }
-};
-
 
 // High level start up sequence
-var http = require('http');                                   // Set up site framework
-arduino.init(process.env.ARDUINO_PORT);                       // serial connect to arduino
-sockets.listen(http);                                         // listen and handle socket connections
-http.listen(process.env.PORT);                                // listen on specified PORT enviornment variable
+arduino.init(process.env.ARDUINO_PORT);                             // serial connect to arduino
+// mongo.init(); might be need if you can build properties in right order
+slack.init(process.env.MASTER_SLACKER, process.env.CONNECT_TOKEN);  // set up connection to our slack intergration server
+auth.init(); // <-- returns that things are connected TODO does that matter ?
