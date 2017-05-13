5881b4d8888d00bde32d7b936eb9f582b4c2377b
Paul Beaudet <inof8or@gmail.com>
2017-05-13 16:35:07 -0400

    Opening a mongo connection and closing it on stream and stream processing done

--- doorboto2.js
+++ doorboto2.js
@@ -2,13 +2,18 @@
 
 var auth = {
     storage: require('node-persist'),
-    updateCard: function(card){ // setItem works like and upsert, this also creates members
-        var cardInfo = {
-            'holder':   card.holder,
-            'expiry':   card.expiry,
-            'validity': card.validity,
+    updateCard: function(onComplete){ // hold on completion function in closure
+        return function(card){        // return a fuction that processes cards
+            var cardInfo = {
+                'holder':   card.holder,
+                'expiry':   card.expiry,
+                'validity': card.validity,
+            };
+            console.log('Updating: ' + JSON.stringify(cardInfo, null, 4));
+            auth.storage.setItem(card.uid, cardInfo) // setItem works like and upsert, this also creates cards
+                .then(function(){onComplete();})     // execute completion callback when done updating cache
+                .catch(function(error){console.log('error updating card: ' + error);});
         };
-        return auth.storage.setItem(card.uid, cardInfo);
     },
     removeCard: function(card){return auth.storage.removeItem(card.uid);}, // return promise card was removed
     orize: function(cardID, onSuccess, onFail){
@@ -36,19 +41,20 @@ var auth = {
         };
     },
     mongoCardCheck: function(scannedCard, onSuccess, onFail){ // hold important top level items in closure
-        return function onConnect(){                          // return a callback to execute on connection to mongo
-            mongo.cards.findOne({id: scannedCard.uid}, function onCard(error, card){
+        return function onConnect(onFinish){                  // return a callback to execute on connection to mongo
+            mongo.cards.findOne({uid: scannedCard.uid}, function onCard(error, card){
                 if(error){
                     console.log('mongo findOne error: ' + error);
                     auth.localCheck(scannedCard, onSuccess, onFail)(); // if there is some sort or read error fallback to local data
                 } else if(card){
                     auth.recordCheck(card, onSuccess, onFail, true);
-                    auth.updateCard(card);                             // keep local redundant data cache up to date
+                    auth.updateCard()(card);                           // keep local redundant data cache up to date
                 } else {
                     onFail('unregistered card');
                     mongo.saveTheRejects(scannedCard.uid);
                 }
             });
+            onFinish();
         };
     },
     recordCheck: function(card, onSuccess, onFail, mongoConnected){            // checks status of card on record
@@ -69,8 +75,10 @@ var auth = {
 
 var cron = {  // runs a time based update opperation
     ONE_DAY: 86400000,
+    progressCount: 0,
+    done: false,
     init: function(hourToUpdate){
-        mongo.connectAndDo(cron.stream, cron.failCase);        // run an initial sync up on start
+        mongo.connectAndDo('cards', cron.stream, cron.failCase); // run an initial sync up on start
         var runTime = cron.millisToHourTomorrow(hourToUpdate); // gets millis till this hour tomorrow
         setTimeout(cron.update, runTime);                      // schedual checks daily for warnigs at x hour from here after
     },
@@ -78,11 +86,29 @@ var cron = {  // runs a time based update opperation
         mongo.connectAndDo(cron.stream, cron.failCase);        // connect to mongo and start an update stream
         setTimeout(cron.update, cron.ONE_DAY);                 // make upcomming expiration check every interval
     },
-    stream: function(){                                        // creates stream of id cards on record to update from
-        var cursor = mongo.card.find({}).cursor();
-        cursor.on('data', auth.updateCard);                    // use local persitence update function to sync with source of truth
+    checkFinished: function(close){
+        if(cron.progressCount === 0 && cron.done){             // given we've made all updates and stream closed
+            close();                                           // reduce risk of having a open connection durring an outage
+            cron.done = false;                                 // reset for next update
+        }
     },
-    failCase: function(){console.log('Failed to update local datastore');},
+    markProgress: function(close){                             // returns data processing success callback
+        cron.progressCount++;                                  // increase count for outstanding jobs
+        return function afterProcessingData(){                 // data processing success callback
+            cron.progressCount--;                              // note processed job
+            cron.checkFinished(close);                         // closes connetion if finished
+        };
+    },
+    stream: function(dbModel, close){                          // Passed dbModel and close event to call when done
+        var cursor = dbModel.find({}).cursor();                // grab cursor to parse through all cards
+        cursor.on('data', auth.updateCard(cron.markProgress(close))); // local persitence update function to sync w/ source of truth
+        cursor.on('close', function(){                         // if I thought about this more carefully closing mongo here would be ok
+            cron.done = true;                                  // flag to allow job to complete
+            console.log('finished stream');
+            cron.checkFinished(close);                         // if outstanding jobs are complete run finished action, prob not now
+        });
+    },
+    failCase: function(error){console.log('Failed to update local datastore: ' + error);},
     millisToHourTomorrow: function(hour){
         var currentTime = new Date().getTime();         // current millis from epoch
         var tomorrowAtX = new Date();                   // create date object for tomorrow
@@ -94,38 +120,40 @@ var cron = {  // runs a time based update opperation
 
 var mongo = { // depends on: mongoose
     ose: require('mongoose'),
-    options: null,                                                             // this is where one would normally put auth info and so on
-    init: function(mongoUri){
-        mongo.uri = mongoUri;
-        mongo.Schema = mongo.ose.Schema;
-        mongo.cards = mongo.ose.model('card', new mongo.Schema({               // Read only by doorboto -- Write only by Interface
-            id: mongo.Schema.ObjectId,
+    uri: process.env.MONGO_URI,
+    cards: function(){
+        return new mongo.ose.Schema({          // Read only by doorboto -- Write only by Interface
+            id: mongo.ose.Schema.ObjectId,
             uid: {type: String, required: '{PATH} is required', unique: true}, // UID of card, collection find key
             holder: {type: String, required: '{PATH} is required'},            // for quickly leaving messages about dated cards without looking up member Maybe not needed
             memberID: {type: String, required: '{PATH} is required'},          // _id of member object fastest way to acurately look up a member
             cardToken: {type: String},                                         // 8 byte QID unique to member, proposing to use in conjunction with UID
             expiry: {type: Number},                                            // expiration of this member
             validity: {type: String, required: '{PATH} is required'}           // activeMember, nonMember, expired, revoked, lost, stolen
-        }));
-        mongo.rejections = mongo.ose.model('rejections', new mongo.Schema({    // list of rejected cards, interface can grab last to register new members instead of using socket.io Write only by doorboto
-            id: mongo.Schema.ObjectId,
+        });
+    },
+    rejections: function(){
+        return new mongo.ose.Schema({// list of rejected cards, interface can grab last to register new members instead of using socket.io Write only by doorboto
+            id: mongo.ose.Schema.ObjectId,
             uid: {type: String, required: '{PATH} is required'},
             holder: {type: String},                                            // if in db note member this could change if card are reused
             validity: {type: String, required: '{PATH} is required'},          // validity at time of scan, set to unregistered if not in db
             timeOf: {type: Date, default: Date.now}
-        }));
-    },
-    connectAndDo: function(success, fail){
-        mongo.ose.connect(mongo.uri, mongo.options, function onConnect(error){
-            if(error){
-                console.error('Mongo issue:' + error);
-                fail(error);
-            } else {
-                success(function onSuccess(){mongo.ose.connection.close();});  // close connection when we are done
-            }
         });
     },
-    saveTheRejects: function(card){
+    connectAndDo: function(collection, success, fail){
+        var connection = mongo.ose.createConnection(mongo.uri);
+        var dbModel = connection.model(collection, mongo[collection]());
+        connection.on('connected', function(){
+            success(dbModel, function callClose(){
+                connection.close();
+            });
+        });
+        connection.on('disconnected', function(){
+            console.log('disconnected from db');
+        });
+    },
+    saveTheRejects: function(card){  // TODO will not work in current situation
         var cardToSave = { // make sure we are only saving feilds that are in our schema, though this might be schema's job
             uid: card.uid,
             holder: card.holder,
@@ -156,7 +184,7 @@ var slack = {
                     token: authToken,
                     slack: {
                         username: 'Doorboto2',
-                        // channel: 'whos_at_the_space',
+                        // channel: 'whos_at_the_space', // TODO COMMENT THIS IN WHEN YOU DEPLOY IDIOT
                         channel: 'test_channel',
                         iconEmoji: ':robot_face:'
                     }
@@ -180,8 +208,7 @@ var arduino = {                          // does not need to be connected to and
     RETRY_DELAY: 5000,
     serialport: require('serialport'),   // yun DO NOT NPM INSTALL -> opkg install node-serialport, use global lib
     init: function(arduinoPort){
-        console.log(arduinoPort);
-        arduino.serial = new arduino.serialport(arduinoPort, {
+        arduino.serial = new arduino.serialport.SerialPort(arduinoPort, {
             baudrate: 9600,              // remember to set you sketch to go this same speed
             parser: arduino.serialport.parsers.readline('\n'),
             autoOpen: false
@@ -191,11 +218,10 @@ var arduino = {                          // does not need to be connected to and
         arduino.serial.on('close', arduino.close);
         arduino.serial.on('error', arduino.error);
     },
-    open: function(){console.log('connected to something');},                   // what to do when serial connection opens up with arduino
-    read: function(data){                                                       // getting data from Arduino, only expect a card ID
-        var id = data.slice(0, data.length-1);                                  // exclude newline char from card ID
-        var authFunction = auth.orize(arduino.grantAccess, arduino.denyAccess); // create authorization function
-        authFunction(id);                                                       // use authorization function
+    open: function(){console.log('connected to something');},    // what to do when serial connection opens up with arduino
+    read: function(data){                                        // getting data from Arduino, only expect a card ID
+        var id = data.slice(0, data.length-1);                   // exclude newline char from card ID
+        auth.orize(id, arduino.grantAccess, arduino.denyAccess); // create authorization function
     },
     close: function(){arduino.init();},                 // try to re-establish if serial connection is interupted
     error: function(error){                             // given something went wrong try to re-establish connection
@@ -212,8 +238,9 @@ var arduino = {                          // does not need to be connected to and
 };
 
 // High level start up sequence
-arduino.init(process.env.ARDUINO_PORT);                             // serial connect to arduino
-mongo.init(process.env.MONGO_URI);                                  // set up mongo schemas
-cron.init();                                                        // run a time based stream that updates local cache
-slack.init(process.env.MASTER_SLACKER, process.env.CONNECT_TOKEN);  // set up connection to our slack intergration server
-auth.storage.init();                                                // TODO return promise that local data store is ready
+auth.storage.init()                                                         // set up local cache
+    .then(function localCacheIsUp(){                                        // when local cache is
+        // arduino.init(process.env.ARDUINO_PORT);                             // serial connect to arduino
+        cron.init(3);                                // run a time based stream that updates local cache
+        // slack.init(process.env.MASTER_SLACKER, process.env.CONNECT_TOKEN);  // set up connection to our slack intergration server
+    });
