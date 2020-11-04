c724751819a168249d080f551cb4195fbdb03069
Paul Beaudet <inof8or@gmail.com>
2020-11-04 12:00:21 -0500

    Intergrate reject into authorize

--- onsite_auth_server/doorboto.mjs
+++ onsite_auth_server/doorboto.mjs
@@ -4,26 +4,8 @@ import { cacheSetup, updateCard, checkForCard } from './storage/on_site_cache.mj
 import { serialInit, acceptSignal, denySignal } from './hardware_interface/reader_com.mjs';
 import { slackSend } from './outward_telemetry/slack.mjs';
 
-const HOUR = 3600000; // an hour in milliseconds
+const HOUR = 3600000;       // milliseconds in an hour
 const LENIENCY = HOUR * 72; // give 3 days for a card to be renewed
-
-// collection of methods that write to makerspace database
-const reject = async (card, db) => {
-  const {uid, holder, validity} = card;
-  const rejectDoc = {
-    uid,
-    // we only get holder when a recorded card holder is rejected
-    holder: holder ? holder : null,
-    // important to know this is an unregistered card if info missing
-    validity: validity ? validity : 'unregistered',
-    timeOf: new Date(), // should be same as mongoose default
-  };
-  try {
-    await db.collection('rejections').insertOne(insertDoc(rejectDoc));
-  } catch (error){
-    console.log(`${uid} rejected but not saved => ${error}`);
-  }
-}
 
 // is called on failed authorization
 const denyAccess = (msg, member = null) => {
@@ -41,48 +23,50 @@ const denyAccess = (msg, member = null) => {
 const authorize = async uid => {
   let mongo = null;
   try {
-    const dbPromise = connectDB();
+    const dbPromise = connectDB(); // Continue while db connects
     let cardData = await checkForCard(uid);
+    let denyMsg = null;
     if (!cardData){ // given no cache entry check db for one
       mongo = await dbPromise;
       cardData = await mongo.db.collection('cards').findOne({ uid });
-      if(!cardData){  // no card here either, this card is unregistered
-        denyAccess('unregistered card');
-        // we want these to show up in the db to register new cards
-        reject({ uid }, mongo.db);
-        return;
+      if(cardData){ // Bring cache up to date with db if a result exist
+        updateCard(cardData);
+      } else { // Unregistered cards are recorded to register new cards
+        cardData = {
+          uid,
+          holder: null,
+          validity: 'unregistered',
+          expiry: 0,
+        }
       }
-      updateCard(cardData); // Bring cache up to date with db
     }
     const {validity, holder, expiry} = cardData;
-    // make sure card has been marked with a valid state
-    let denyMsg = null;
+    // make sure card has been marked with a valid state and unexpired
     if (validity === 'activeMember' || validity === 'nonMember') {
-      // make sure card is not expired
       if (new Date().getTime() < new Date(expiry).getTime() + LENIENCY) {
         acceptSignal(); // Trigger the door strike to open
         slackSend(`${holder} just checked in`);
-        const checkinDoc = {
-          name: holder,
-          time: new Date().getTime(),
-        };
-        if(!mongo){ mongo = await dbPromise; }
-        await mongo.db.collection('checkins').insertOne(insertDoc(checkinDoc));
       } else { // Note that this denial was because of a lapsed membership
         denyMsg = `${holder}'s membership as lapsed`;
       }
     } else { // Note this denial was a validity issue
       denyMsg = `${holder}'s  ${validity} card was scanned`;
     }
+    if(!mongo){ mongo = await dbPromise; }
     if(denyMsg){
       denyAccess(denyMsg, holder);
-      if(!mongo){ mongo = await dbPromise; }
-      reject(cardData, mongo.db);
+      cardData.timeOf = new Date();
+      await mongo.db.collection('rejections').insertOne(insertDoc(cardData));
+    } else {
+      const checkinDoc = {
+        name: holder,
+        time: new Date().getTime(),
+      };
+      await mongo.db.collection('checkins').insertOne(insertDoc(checkinDoc));
     }
-    if(!mongo){ mongo = await dbPromise; }
     mongo.closeDb();
   } catch (error){
-    const issue = `${uid} rejected due to amnesia => ${error}`;
+    const issue = `${uid} rejected due to system error => ${error}`;
     denyAccess(issue);
     console.log(issue);
   }
