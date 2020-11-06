504256a539102f977932378055b2c7c1130965cd
Paul Beaudet <inof8or@gmail.com>
2020-11-06 16:09:46 -0500

    status cake heartbeat ping draft

---
+++ onsite_auth_server/outward_telemetry/statusCake.js
@@ -0,0 +1,32 @@
+const { request } = require('https');
+const DEFAULT_POST = process.env.STATUS_CAKE_KEY || ''
+
+// non-functional: Putting this in here to remind us to set up a heartbeat
+
+
+const pingStatusCake = (apiInfo = DEFAULT_POST) => {
+  return new Promise((resolve) => {
+    if(!apiInfo){
+      return;
+    }
+    const postData = apiInfo;
+    const options = {
+      hostname: 'push.statuscake.com',
+      port: 443,
+      method: 'POST',
+      apiInfo,
+      headers: {
+        'Content-Type': 'application/url',
+        'Content-Length': postData.length,
+      },
+    };
+    const req = request(options, resolve);
+    // just do it, no need for response
+    req.write(postData);
+    req.end();
+  });
+};
+
+module.exports = {
+  pingStatusCake,
+}
