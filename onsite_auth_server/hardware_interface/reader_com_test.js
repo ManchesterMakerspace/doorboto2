9cb0bf88e3764ae1322215aec73c58cbf0c65c69
Paul Beaudet <inof8or@gmail.com>
2020-11-12 09:36:17 -0500

    Update reader test to account for new authorize flow

--- onsite_auth_server/hardware_interface/reader_com_test.js
+++ onsite_auth_server/hardware_interface/reader_com_test.js
@@ -1,16 +1,16 @@
 // reader_com_test.js Copyright 2020 Manchester Makerspace MIT License
 
-const { serialInit, giveAccess } = require('./reader_com.js');
+const { serialInit } = require('./reader_com.js');
 
 const canItGrantAccess = () => {
-  serialInit(data => {
+  serialInit( (data, giveAccess) => {
     console.log(data);
     giveAccess(true);
   });
 };
 
 const canItDenyAccess = () => {
-  serialInit(data => {
+  serialInit( (data, giveAccess) => {
     console.log(data);
     giveAccess(false);
   });
